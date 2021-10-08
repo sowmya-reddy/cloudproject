e020c98a7120a523d050db3ddbc690497b21f9ea
sowmya-reddy <kjsowmya97@gmail.com>
2021-10-08 14:35:52 -0400

    Add files via upload

---
+++ tcpclient.py
@@ -0,0 +1,35 @@
+from socket import *
+import pickle
+import threading
+
+serverName = 'localhost'
+serverPort = 9890
+
+def threaded_clients(clientSocket):
+    clientSocket.connect((serverName,serverPort))
+    print("Connected to server. Use commands 'set' and 'get' to retrieve or store key value pairs. Use Ctrl D in new line to exit command line and 'quit' to close the connection")
+    close_flag=False
+    while True:
+        user_writing = [] 
+        while True: 
+           try:
+               cmd=input()
+               if(cmd=='quit'):
+                    close_flag=True
+                    break
+               user_writing.append(cmd)
+           except EOFError:
+               break
+        if(close_flag):
+            break
+        data = pickle.dumps(user_writing)
+        clientSocket.sendall(data)
+        data=user_writing[0].split()
+        value = clientSocket.recv(1024)
+        print(value.decode("utf-8"))
+    print("Connection closed")
+    clientSocket.close()
+  
+clientSocket = socket(AF_INET, SOCK_STREAM)
+t = threading.Thread(target=threaded_clients, args=(clientSocket,))
+t.start()
---
+++ tcpserver.py
@@ -0,0 +1,65 @@
+from socket import *
+import pickle
+from _thread import *
+
+host = '127.0.0.1' 
+ThreadCount = 0
+serverPort = 9890
+serverSocket = socket(AF_INET,SOCK_STREAM)
+serverSocket.bind((host,serverPort))
+serverSocket.listen(1)
+print('The server is ready to receive')
+
+def threaded_client(connectionSocket):
+    while True:
+        try:
+            data = connectionSocket.recv(1024)
+            data1 = pickle.loads(data)
+        except EOFError:
+            print("Connection to client closed")
+            connectionSocket.close()
+            break
+        command = data1[0].split()
+        if(command[0] == 'set'):
+            key = command[1]
+            value = data1[1]
+            size = command[2]
+            dictionary={}
+            if(len(value) == int(size)):
+                try: 
+                    with open('data.pickle', 'rb') as file:
+                        dictionary = pickle.load(file)
+                except:
+                    tempDict = {'key': 'value'}
+                    with open('data.pickle', 'wb') as file:
+                        pickle.dump(tempDict,file)
+                       
+                dictionary[key] = value
+                with open('data.pickle', 'wb') as file:
+                    pickle.dump(dictionary,file)
+                message = 'STORED'
+            else:
+                message = 'NOT STORED'
+            connectionSocket.send(message.encode())
+            
+        elif(command[0] == 'get'):
+            key = command[1]
+            with open('data.pickle', 'rb') as file:
+                 dictionary = pickle.load(file)
+            if key not in dictionary:
+                connectionSocket.send(str.encode('VALUE '+key+' '+'\n'+'NOT FOUND\n'+'END'))
+            else:
+                value = dictionary[key]
+                connectionSocket.send(str.encode('VALUE '+key+' '+str(len(value))+'\n'+value+'\n'+'END'))
+        else:
+            message='ERROR!'
+            connectionSocket.send(message.encode())
+    connectionSocket.close()
+
+while 1:
+    connectionSocket, addr = serverSocket.accept()
+    print('Connected to: ' + addr[0] + ':' + str(addr[1]))
+    start_new_thread(threaded_client, (connectionSocket, ))
+    ThreadCount += 1
+    print('Thread Number: ' + str(ThreadCount))
+serverSocket.close()
